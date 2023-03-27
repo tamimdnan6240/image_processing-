37b9e985cdae9a5313dc7300b89eb7288ef06262
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2023-03-26 21:48:52 -0400

    Update dice_index.py

--- dice_index.py
+++ dice_index.py
@@ -2,13 +2,14 @@ def dice_coefficient(pred, mask):
     smooth = 1e-5 
     pred = pred.view(-1)
     mask = mask.view(-1)
-    intersection = (pred * mask).sum() * 2
-    union = pred.sum() + mask.sum()
-    accuracy = (intersection + smooth) / (union + smooth) 
+    intersection = (pred * mask).sum()
+    union = pred.sum() + mask.sum() + smooth
+    accuracy = (2 * intersection + smooth) / union
     print("Intersection:", intersection)
     print("Union:", union)
     print("Dice coefficient:", accuracy)
     return accuracy
+
 
 import torch
 pred = torch.Tensor([0, 1, 1, 0, 1])
