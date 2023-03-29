f53a15d5492c5574a21fde65d8f6625df63eedba
tadnan <105733511+tamimdnan6240@users.noreply.github.com>
2023-03-28 23:55:38 -0400

    Create rotation512x512.py

---
+++ Augmentations/Resize_512x512_&_Rotation_CRACKTREE260/rotation512x512.py
@@ -0,0 +1,19 @@
+import torch
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import cv2 
+from matplotlib import pyplot as plt
+import numpy as np
+from torchvision import transforms, models
+import matplotlib.image as mpimg 
+from PIL import Image
+import numpy as np
+from numpy import asarray
+import glob
+import os
+import imageio
+import imgaug as ia
+from imgaug import augmenters as iaa
+
+import os
+from PIL import Image
